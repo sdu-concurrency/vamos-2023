1fd44361088ca96fdc2e6e134972d7076f59e065
lovrosdu <lugovic@imada.sdu.dk>
2022-11-23 16:12:18 +0100

    Untabify

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker run -it --rm \
-	   -v "$(pwd)"/website/_site:/var/lib/leonardo/www \
-	   -p 8080:8080 \
-	   jolielang/leonardo
+       -v "$(pwd)"/website/_site:/var/lib/leonardo/www \
+       -p 8080:8080 \
+       jolielang/leonardo
